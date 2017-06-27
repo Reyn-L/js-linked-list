3cd7d0ecbed66dcd55043fe5dc54d06852907264
Reyn-L <Reyn.Leong@gmail.com>
2017-06-27 11:28:18 -1000

    gethead

--- linkedList.js
+++ linkedList.js
@@ -1,8 +1,22 @@
+/*jshint esversion: 6*/
 /**
  * @name  linkedListGenerator
  * @description  Main Module
  * @return {Object} an object exposing methods to be used to manipulate a linked list
  */
-function linkedListGenerator(){
-return {};
+ function linkedListGenerator(){
+  let linkedList;
+  let module = {};
+
+function getHead() {
+
+}
+
+function getTail() {
+
+}
+
+  return {
+    getHead : getHead
+  };
 }
