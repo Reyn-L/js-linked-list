af67bccc82c77cd2289f1af32c6b995842a76c36
Reyn-L <Reyn.Leong@gmail.com>
2017-06-27 20:11:40 -1000

    add, tail, get, struggling

--- linkedList.js
+++ linkedList.js
@@ -5,18 +5,98 @@
  * @return {Object} an object exposing methods to be used to manipulate a linked list
  */
  function linkedListGenerator(){
-  let linkedList;
-  let module = {};
 
-function getHead() {
+   let linkedList = null;
 
-}
+   function getHead() {
+    return linkedList;
+  }
 
-function getTail() {
+  function getTail() {
+    let currentNode = linkedList;
 
-}
+    if(!currentNode) {
+      return null;
+
+    } while (currentNode.next) {
+      currentNode = currentNode.next;
+    }
+    return currentNode;
+  }
+
+  function add(value) {
+    let node = {
+      value : value,
+      next: null
+    };
+
+    let currentNode = linkedList;
+
+    if (currentNode === null) {
+      linkedList = node;
+      return node;
+
+    }else{
+    } while (currentNode.next) {
+      currentNode = currentNode.next;
+    }
+    currentNode.next = node;
+    return node;
+  }
+
+  function remove(number) {
+    let currentNode = get(number);
+    let previousNode = get(number - 1);
+    console.log(previousNode);
+    for (let i = 0; i < number; i++){
+      previousNode.next = currentNode.next;
+    }
+      if(currentNode.next === null){
+        return false;
+      }
+    // while(number < currentNode.length) {
+    //   if (currentNode[number].next === null){
+    //     return false;
+    //   }
+    //   number++;
+    // }
+
+    return currentNode.next;
+  }
+
+  function get(number) {
+    let currentNode = linkedList;
+
+    for (let i = 0; i < number; i++) {
+      if(currentNode.next === null) {
+        return false;
+      }
+      currentNode = currentNode.next;
+    }
+
+    return currentNode;
+  }
+
+  function insert() {
+
+  }
 
   return {
-    getHead : getHead
+    getHead : getHead,
+    getTail : getTail,
+    add : add,
+    remove : remove,
+    get : get,
+    insert : insert
   };
 }
+// console.log(ll.getHead()); //null
+// ll.add('cat');
+// console.log(ll.getHead()); //cat
+// console.log(ll.getTail()); //cat
+// ll.add('Dog');
+// console.log(ll.getTail()); //dog
+// ll.insert('bird',1);
+// console.log(ll.get(1)); //bird
+// ll.remove(2);
+// console.log(ll.getTail()); //bird
